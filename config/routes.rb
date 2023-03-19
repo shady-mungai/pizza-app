0dfc0652d461858fecd30cebe7ac8df522574a86
shady-mungai <shadrackmungai10@gmail.com>
2023-03-20 01:22:08 +0300

    Added necessary routes to the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :restaurant_pizzas, only: [:index,:create]
+  resources :pizzas, only: [:index]
+  resources :restaurants, only: [:index,:show,:destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
